61e0cdc70c6c62abb00971b3987122c1d2bb12c6
Manjunatth <33751912+nbms12@users.noreply.github.com>
2025-02-26 01:16:11 +0530

    Update server.js

--- project2_EKS_Deployment/server.js
+++ project2_EKS_Deployment/server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World, Manjunat here");
+  res.send("Hello World, Manjunath here");
 });
 
 app.listen(PORT, HOST, () => {
