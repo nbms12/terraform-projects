261bb0663e04068e776de1f0e1ec22a82b171292
Manjunath nb <33751912+nbms12@users.noreply.github.com>
2025-07-30 20:55:12 +0530

    Update provider.tf

--- project- terraform-with-azure/day8/provider.tf
+++ project- terraform-with-azure/day8/provider.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id       = "21d92bdb-6bc0-47f4-8ca1-8ccb03156393"
-  subscription_id = "53e6965c-8261-4665-9d64-e8c07f50510f"
-  client_id       = "f74562df-824a-4bfc-ab71-614526f09fa4"
-  client_secret   = "STp8Q~vxbk~aBQUY5wCA9lg51rpPvr5-fVdPGcbH"
+  tenant_id       = "xxxxxxxxxxxx8ca1-8ccb03156393"
+  subscription_id = "53e6xxxxxxxxxxxxxxxxxxxx50510f"
+  client_id       = "xxxxxxxxxxxxxxxxxxx4"
+  client_secret   = "xxxxxxxxxxxxxxxxxxcbH"
 
 
 }
