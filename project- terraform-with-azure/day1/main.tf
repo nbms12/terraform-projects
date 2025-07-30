9dcb4881cd9e2bb7c42616deb37c91b4b763a8d4
Manjunath nb <33751912+nbms12@users.noreply.github.com>
2025-07-30 20:15:33 +0530

    Update main.tf

--- project- terraform-with-azure/day1/main.tf
+++ project- terraform-with-azure/day1/main.tf
@@ -11,9 +11,9 @@ terraform {
 provider "azurerm" {
   features {}
   tenant_id = "21d92bdb-6bc0-47f4-8ca1-8ccb03156393"
-  # subscription_id = "53e6965c-8261-4665-9d64-e8c07f50510f"
-  # client_id       = "f74562df-824a-4bfc-ab71-614526f09fa4"
-  # client_secret   = "STp8Q~vxbk~aBQUY5wCA9lg51rpPvr5-fVdPGcbH"
+  # subscription_id = "......53e6965c-8261-4665-9d64........"
+  # client_id       = ".....f74562df-824a......"
+  # client_secret   = "....STp8Q~vxbk~aBQUY5wCA...."
 
 
 }
